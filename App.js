4c047f03af9d7ce4ed0e00992967a998b1a102e6
kryptkode <paulakhamiogu@gmail.com>
2020-11-02 04:37:20 +0100

    with proper styling

--- App.js
+++ App.js
@@ -7,21 +7,22 @@ const App: () => React$Node = () => {
     <>
       <StatusBar />
       <SafeAreaView style={styles.container}>
-        <Text style={[styles.text, { fontWeight: 'bold', color: '#000' }]}>
+        <Text style={styles.heading}>
           Here are some boxes of different colors
         </Text>
-        <Text style={[styles.text, { backgroundColor: '#2aa198' }]}>
-          Cyan #2aa198
-        </Text>
-        <Text style={[styles.text, { backgroundColor: '#268bd2' }]}>
-          Blue #268bd2
-        </Text>
-        <Text style={[styles.text, { backgroundColor: '#d33682' }]}>
-          Magenta #d33682
-        </Text>
-        <Text style={[styles.text, { backgroundColor: '#cb4b16' }]}>
-          Orange #cb4b16
-        </Text>
+        <View style={[styles.box, styles.cyan]}>
+          <Text style={styles.text}>Cyan #2aa198</Text>
+        </View>
+
+        <View style={[styles.box, styles.blue]}>
+          <Text style={styles.text}>Blue #268bd2</Text>
+        </View>
+        <View style={[styles.box, styles.magenta]}>
+          <Text style={styles.text}>Magenta #d33682</Text>
+        </View>
+        <View style={[styles.box, styles.orange]}>
+          <Text style={styles.text}>Orange #cb4b16</Text>
+        </View>
       </SafeAreaView>
     </>
   );
@@ -29,18 +30,39 @@ const App: () => React$Node = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'column',
+    paddingTop: 50,
+    paddingHorizontal: 10,
+  },
+
+  heading: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: 'black',
+    marginBottom: 10,
   },
   text: {
-    padding: 15,
-    marginVertical: 10,
-    marginHorizontal: 10,
-    borderRadius: 5,
-    textAlign: 'center',
-    textAlignVertical: 'center',
-    color: '#fff',
-    fontWeight: '500',
+    fontWeight: 'bold',
     fontSize: 15,
+    color: 'white',
+  },
+  box: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    marginVertical: 5,
+    borderRadius: 5,
+  },
+  cyan: {
+    backgroundColor: '#2aa198',
+  },
+  blue: {
+    backgroundColor: '#268bd2',
+  },
+  magenta: {
+    backgroundColor: '#d33682',
+  },
+  orange: {
+    backgroundColor: '#cb4b16',
   },
 });
 
