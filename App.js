048c43bfbbdde96b906a130ab8e6a2ac1d779ec4
kryptkode <paulakhamiogu@gmail.com>
2020-11-02 04:27:00 +0100

    create color app

--- App.js
+++ App.js
@@ -1,29 +1,46 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, SafeAreaView, StatusBar } from 'react-native';
 
 const App: () => React$Node = () => {
   console.log('Hello world');
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello</Text>
-      <Text style={styles.text}> World</Text>
-    </View>
+    <>
+      <StatusBar />
+      <SafeAreaView style={styles.container}>
+        <Text style={[styles.text, { fontWeight: 'bold', color: '#000' }]}>
+          Here are some boxes of different colors
+        </Text>
+        <Text style={[styles.text, { backgroundColor: '#2aa198' }]}>
+          Cyan #2aa198
+        </Text>
+        <Text style={[styles.text, { backgroundColor: '#268bd2' }]}>
+          Blue #268bd2
+        </Text>
+        <Text style={[styles.text, { backgroundColor: '#d33682' }]}>
+          Magenta #d33682
+        </Text>
+        <Text style={[styles.text, { backgroundColor: '#cb4b16' }]}>
+          Orange #cb4b16
+        </Text>
+      </SafeAreaView>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
+    flexDirection: 'column',
   },
   text: {
-    backgroundColor: 'lavender',
-    flex: 1,
-    padding: 20,
+    padding: 15,
+    marginVertical: 10,
+    marginHorizontal: 10,
+    borderRadius: 5,
     textAlign: 'center',
     textAlignVertical: 'center',
-    marginVertical: 30,
-    marginHorizontal: 10,
-    minHeight: 100,
+    color: '#fff',
+    fontWeight: '500',
+    fontSize: 15,
   },
 });
 
